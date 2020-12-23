c31b12f0b354aaecf5600b4be1f922d47150b530
JL-OLEMAR <joseluis19963@gmail.com>
2020-12-22 19:40:43 -0500

    User Updated Method

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,16 +2,17 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Componentes
-<<<<<<< HEAD
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
+  { path: 'mis-datos', component: UserEditComponent },
 ];
 
 export const appRoutingProviders: any[] = [];
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -68,9 +68,22 @@ export class UserService {
       .set('Authorization', this.getToken());
 
     if (userId != null) {
-      return this._http.get(this.url + 'counters/' + userId, { headers: headers });
+      return this._http.get(this.url + 'counters/' + userId, {
+        headers: headers,
+      });
     } else {
       return this._http.get(this.url + 'counters', { headers: headers });
     }
+  }
+
+  updateUser(user): Observable<any> {
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', this.getToken());
+
+    return this._http.put(this.url + 'update-user/' + user._id, params, {
+      headers: headers,
+    });
   }
 }
